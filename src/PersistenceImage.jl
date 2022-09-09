45409ca1184d70e7a5a989fc6009bc6895be4814
ed19aaf <e.dmitruk@herts.ac.uk>
2022-09-09 15:22:23 +0100

    Reformatted the code

--- src/PersistenceImage.jl
+++ src/PersistenceImage.jl
@@ -13,25 +13,25 @@ include("auxiliaryfunctions.jl")
 Return the persistence image of a persistence diagram, given the
 persistence diagram, pixels and standard deviation.
 """
-function transformdiagram(diagram::Array{Float64, 2};
-                          pixels::Tuple{Int64,Int64}=(10,10), σ::Float64 = 1.)
+function transformdiagram(diagram::Array{Float64,2};
+    pixels::Tuple{Int64,Int64}=(10, 10), σ::Float64=1.0)
 
     landscape = tolandscape(diagram)
     # min and max in the x axis of the landscape
-    xmin = minimum([minimum(landscape[:,1]), 0])
-    xmax = maximum(landscape[:,1])
+    xmin = minimum([minimum(landscape[:, 1]), 0])
+    xmax = maximum(landscape[:, 1])
     # min and max in the y axis of the landscape
-    ymin = minimum([minimum(landscape[:,2]), 0])
-    ymax = maximum(landscape[:,2])
+    ymin = minimum([minimum(landscape[:, 2]), 0])
+    ymax = maximum(landscape[:, 2])
     # create a grid over our landscape
-    xaxis = range(xmin, stop=xmax, length=pixels[1]+1)
-    yaxis = range(ymin, stop=ymax, length=pixels[2]+1)
+    xaxis = range(xmin, stop=xmax, length=pixels[1] + 1)
+    yaxis = range(ymin, stop=ymax, length=pixels[2] + 1)
 
     img = Array{Float64}(undef, pixels[1], pixels[2])
 
     for i in 1:size(landscape, 1)
-        birth = landscape[i,1]
-        persistence = landscape[i,2]
+        birth = landscape[i, 1]
+        persistence = landscape[i, 2]
         weights = weighting(birth, persistence)
         dB = Normal(birth, σ)
         dP = Normal(persistence, σ)
@@ -45,7 +45,7 @@ function transformdiagram(diagram::Array{Float64, 2};
                 xpdf = pdf(dB, xcenter)
                 ypdf = pdf(dP, ycenter)
                 # sum to the corresponding pixel
-                img[size(img,1)+1-k,j] += weights * xpdf * ypdf
+                img[size(img, 1)+1-k, j] += weights * xpdf * ypdf
             end
         end
     end
@@ -55,44 +55,43 @@ end
 """
     transformdiagram2(diagram; pixels, σ, birth_range, death_range)
 """
-function transformdiagram2(diagram::Array{Float64, 2};
-                          pixels::Tuple{Int64,Int64}=(10,10),
-                          σ::Float64 = 1.,
-                          birth_range::Tuple{Float64, Float64}=(min(diagram[:,1]...),max(diagram[:,1]...)),
-                          death_range::Tuple{Float64, Float64}=(min(diagram[:,2]...),max(diagram[:,2]...)),
-                                                                                     )
+function transformdiagram2(diagram::Array{Float64,2};
+    pixels::Tuple{Int64,Int64}=(10, 10),
+    σ::Float64=1.0,
+    birth_range::Tuple{Float64,Float64}=(min(diagram[:, 1]...), max(diagram[:, 1]...)),
+    death_range::Tuple{Float64,Float64}=(min(diagram[:, 2]...), max(diagram[:, 2]...))
+)
     landscape = tolandscape2(diagram)
-    births = diagram[:,1]
-    persistances = diagram[:,2] - diagram[:,1]
+    births = diagram[:, 1]
+    persistances = diagram[:, 2] - diagram[:, 1]
 
     # create a grid over our landscape
-    landscape_space = tolandscape2(diagram)
     space_diagram = [birth_range[1] death_range[1]
-                        birth_range[1] death_range[2]
-                        birth_range[2] death_range[2]]
+        birth_range[1] death_range[2]
+        birth_range[2] death_range[2]]
     landscape_space = tolandscape2(space_diagram)
-    x_start = min(landscape_space[:,1]...)
-    x_end= max(landscape_space[:,1]...)
+    x_start = min(landscape_space[:, 1]...)
+    x_end = max(landscape_space[:, 1]...)
     y_start = min(landscape_space[:, 2]...)
     y_end = max(landscape_space[:, 2]...)
 
     xaxis = range(x_start, x_end, length=pixels[1])
     yaxis = range(y_start, y_end, length=pixels[1])
-    img = zeros(Float64, pixels[1], pixels[2] )
+    img = zeros(Float64, pixels[1], pixels[2])
 
     for i in 1:size(landscape, 1)
         birth = births[i]
         persistence = persistances[i]
-        x_coord = landscape[i,1]
-        y_coord = landscape[i,2]
+        x_coord = landscape[i, 1]
+        y_coord = landscape[i, 2]
         weights = weighting(birth, persistence)
 
         dB = Normal(x_coord, σ)
         dP = Normal(y_coord, σ)
         # get a vector of x values
-        x_vals = pdf(dB, xaxis)
-        y_vals = pdf(dP, yaxis)
-        img_slice = (y_vals * (x_vals') * weights)[end:-1:1,:]
+        x_vals = [Distributions.pdf(dB, x) for x in xaxis]
+        y_vals = [Distributions.pdf(dP, y) for y in yaxis]
+        img_slice = (y_vals*(x_vals')*weights)[end:-1:1, :]
         img .+= img_slice
     end
 
